e44ad9eada493e7c422273cd10aa1915a79fadf0
Patrick Elsen <pelsen@xfbs.net>
2024-01-28 14:35:24 -0500

    Updates

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # Tupperware
 
+[![docs](https://img.shields.io/badge/docs-latest-blue)](https://docs.rs/tupperware)
+[![crate version](https://img.shields.io/crates/v/restless.svg)](https://crates.io/crates/tupperware)
+
 Crate that lets you decide if you want to put your types into a `Box`. It allows
 you to express polymorphism over how your data is stored.
 
@@ -14,42 +17,39 @@ differently depending on circumstance. Some examples are:
 - You want to be able to store references to data in circumstances.
 
 Tupperware can help here, by allowing you to use the type system to switch between
-different data storage variations.
+different data storage variations. You can write your struct definitions semantially,
+capturing what data each field should hold, and then switch between different storage
+representations of your data.
 
-For example:
+## Usage
+
+For example, you can define a struct such as this:
 
 ```rust
 use tupperware::{Storage, Inline};
 
+type OrderId = i64;
+
 #[derive(Debug, Clone)]
 struct MyData<S: Storage = Inline> {
     name: S::Value<str>,
-    orders: S::Value<i64>,
+    orders: S::Value<[OrderId]>,
 }
 ```
 
-With this code, you can now decide between storing the data inline (which is
-the default) or storing it in an Arc. For example, `MyData<Inline>` expands to:
+With this code, you can now use the type parameter to control how your types are stored.
+This is set to default to storing the type inline, however you have a couple of built-in
+strategies. Here is an example of how `str` maps using different strategies:
 
-```rust
-struct MyData {
-    name: String,
-    orders: Vec<i64>
-}
-```
+| Strategy | Type |
+| --- | --- |
+| Inline | `String` |
+| Arc | `Arc<str>` |
+| Box | `Box<str>` |
+| Rc | `Rc<str>` |
+| Ref<'a> | `&'a str` |
 
-However, `MyData<Arc>` expands to:
-
-```rust
-struct MyData {
-    name: Arc<str>,
-    orders: Arc<[i64]>
-}
-```
-
-Using this trait gives you polymorphism over how your data is stored. You can
-also defined your own custom storage strategies.
-
+You can also define your own storage mechanisms by implementing the `Storage` trait.
 
 ## License
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,91 @@
-pub mod storage;
+use std::{
+    marker::PhantomData,
+    borrow::Borrow,
+    path::{Path, PathBuf},
+    ffi::{OsStr, OsString},
+};
+
+/// Maps a (potentially unsized) type to it's sized representation.
+///
+/// This trait is similar to the [`ToOwned`] trait except that it does not require the value to be
+/// [`Clone`].
+pub trait SizedRepresentation {
+    /// Sized representation of the value.
+    type Sized: Borrow<Self>;
+}
+
+impl<T> SizedRepresentation for T {
+    type Sized = T;
+}
+
+impl SizedRepresentation for str {
+    type Sized = String;
+}
+
+impl<T> SizedRepresentation for [T] {
+    type Sized = Vec<T>;
+}
+
+impl SizedRepresentation for Path {
+    type Sized = PathBuf;
+}
+
+impl SizedRepresentation for OsStr {
+    type Sized = OsString;
+}
+
+/// Storage kind for values.
+pub trait Storage {
+    /// Type used to store a particular value.
+    type Type<T: 'static + SizedRepresentation + ?Sized>: Borrow<T>;
+}
+
+/// Store the value inline.
+pub struct Inline;
+
+/// Store the value in a heap allocation.
+pub struct Box;
+
+/// Store the value in an atomic reference-counted heap allocation [`Arc`][std::sync::Arc].
+pub struct Arc;
+
+/// Store the value in a reference-counted heap allocation.
+pub struct Rc;
+
+/// Store the value as a reference.
+pub struct Ref<'a>(PhantomData<&'a ()>);
+
+impl Storage for Inline {
+    type Type<T: 'static + SizedRepresentation + ?Sized> = T::Sized;
+}
+
+impl Storage for Box {
+    type Type<T: 'static + SizedRepresentation + ?Sized> = std::boxed::Box<T>;
+}
+
+impl Storage for Arc {
+    type Type<T: 'static + SizedRepresentation + ?Sized> = std::sync::Arc<T>;
+}
+
+impl Storage for Rc {
+    type Type<T: 'static + SizedRepresentation + ?Sized> = std::rc::Rc<T>;
+}
+
+impl<'b> Storage for Ref<'b> {
+    type Type<T: 'static + SizedRepresentation + ?Sized> = &'b T;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[derive(Clone, Debug)]
+    struct MyData<S: Storage> {
+        string: S::Type<str>,
+        set: std::collections::BTreeSet<S::Type<u64>>,
+    }
+
+    type MyDataBoxed = MyData<Box>;
+    type MyDataArc = MyData<Arc>;
+    type MyDataRef<'a> = MyData<Ref<'a>>;
+}
